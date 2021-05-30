c403d83db7da4cdfb9cce148387c40495eb2b125
Zach443 <zbfiorito@gmail.com>
2021-05-29 21:00:32 -0500

    Load classes from config on startup
    Currently does not support enchanted items

--- src/main/java/org/battleplugins/arena/arena/classes/ArenaClassManager.java
+++ src/main/java/org/battleplugins/arena/arena/classes/ArenaClassManager.java
@@ -5,7 +5,12 @@ import mc.alk.battlecore.util.Log;
 import org.battleplugins.api.inventory.item.ItemStack;
 import org.battleplugins.arena.BattleArena;
 import org.battleplugins.arena.arena.player.ArenaPlayer;
+import org.battleplugins.arena.file.configuration.Configuration;
+import org.battleplugins.arena.file.reader.item.ItemReader;
+import org.spongepowered.configurate.ConfigurationNode;
 
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -23,7 +28,25 @@ public class ArenaClassManager {
     private Map<String, ArenaClass> arenaClasses;
 
     public ArenaClassManager(BattleArena plugin) {
+
         this.plugin = plugin;
+        this.arenaClasses = new LinkedHashMap<>();
+
+        Configuration classConfig = plugin.getConfigManager().getClassConfig();
+
+        Map<Object, ? extends ConfigurationNode> nodeMap = classConfig.getNode("classes").getChildrenMap();
+        for (Map.Entry<Object, ? extends ConfigurationNode> classEntry : nodeMap.entrySet()) {
+
+            ConfigurationNode classNode = nodeMap.get(classEntry.getKey());
+
+            List<ItemStack> items = classNode.getNode("items").getList(object -> ItemReader.readItem(object.toString()).orElse(null));
+
+
+            ArenaClass newClass = new ArenaClass(classNode.getNode("displayName").getString(), items);
+            addClass(newClass);
+
+
+        }
     }
 
     /**
